3954a9843d952ad62887d5e3c1cf8d8d94d68e46
wanchengchen <wanchengchen@wanchengchendeMacBook-Pro.local>
2017-02-21 18:35:53 +0800

    Revert "test"
    This reverts commit d17307c0354c63094b8d9ec00608b6f36fb6ddc4.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel——hello
+                    Laravel
                 </div>
 
                 <div class="links">
