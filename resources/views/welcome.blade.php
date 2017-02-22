4ed17030017bb5d0f2b4a9a0971240095f8ef81d
wanchengchen <532488014@qq.ocm>
2017-02-22 11:32:24 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-hello112
+                    Laravel-hello112s
                 </div>
 
                 <div class="links">
