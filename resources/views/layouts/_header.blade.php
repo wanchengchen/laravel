77662a27a41f34a88d0d87a93e8cb5efb4a1dc41
wanchengchen <532488014@qq.ocm>
2017-02-24 16:48:57 +0800

    Finish user signup5

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,7 +1,7 @@
 <header class="navbar navbar-fixed-top navbar-inverse">
     <div class="container">
         <div class="col-md-offset-1 col-md-10">
-            <a href="/" id="logo">Sample App</a>
+            <a href="/" id="logo">Wcc App</a>
             <nav>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="{{ route('help') }}">帮助</a></li>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <title>@yield('title', 'WELCOME') - WanChengCheng Blog</title>
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/default.css">
 </head>
@@ -10,6 +10,7 @@
 
 <div class="container">
     <div class="col-md-offset-1 col-md-10">
+        @include('shared.messages')
         @yield('content')
         @include('layouts._footer')
     </div>
